9c06d8f125469e54998c226eabeda79473fefed7
sharon0812 <anyangosharon@gmail.com>
2020-10-09 16:25:50 +0300

    modify js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,39 @@
-$(document) .ready(function(){
-    $("#design") .click(function(){
-        $("#design") .hide(slow); 
-        $("#pa") .show(slow);
-    });
-    $("#pa") .click(function(){
-        $("#pa").hide(slow); 
-        $("#design").show(slow);
-    });
+// $(document).ready(function(){
+//     $("#design") .click(function(){
+//         $("#design").hide(slow); 
+//         $("#pa").show(slow);
+//     });
+//     $("#pa") .click(function(){
+//         $("#pa").hide(slow); 
+//         $("#design").show(slow);
+//     });
     
+// });
+
+
+$(document).ready(function(){
+    $("#design").click (function(){
+        $("#design").hide("slow");
+        $("#pa").show("slow");
+    });
+    $("#pa").click (function(){
+        $("#pa").hide("slow");
+        $("#design").show("slow");
+    });
+    $("#design").click (function(){
+        $("#design").hide("slow");
+        $("#pa").show("slow");
+    });
+    $("#pa").click (function(){
+        $("#pa").hide("slow");
+        $("#design").show("slow");
+    });
+    $("#design").click (function(){
+        $("#design").hide("slow");
+        $("#pa").show("slow");
+    });
+    $("#pa").click (function(){
+        $("#pa").hide("slow");
+        $("#design").show("slow");
+    });
 });
