6848ee813ff5f62ed705c530b84a97ca23ba2694
sharon0812 <anyangosharon@gmail.com>
2020-10-09 19:57:42 +0300

    add hide functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,3 @@
-// $(document).ready(function(){
-//     $("#design") .click(function(){
-//         $("#design").hide(slow); 
-//         $("#pa").show(slow);
-//     });
-//     $("#pa") .click(function(){
-//         $("#pa").hide(slow); 
-//         $("#design").show(slow);
-//     });
-    
-// });
-
-
 $(document).ready(function(){
     $("#design").click (function(){
         $("#design").hide("slow");
